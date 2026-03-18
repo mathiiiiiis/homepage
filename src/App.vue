cc03a5d23e74a6abbc65e5d4fbe4d51d3e555cef
mathiiiiiis <mathis@mathiiis.de>
2026-03-18 08:31:03 +0100

    add page transition

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,9 @@ usePreloadImages()
 
 <template>
   <IconSprite />
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="page" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
