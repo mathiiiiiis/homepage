ae411c5772d52ea32dac87ddd5da917500058588
mathiiiiiis <mathis@mathiiis.de>
2026-03-18 01:02:55 +0100

    make home the root and add style vals

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
 import { usePreloadImages } from '@/composables/usePreloadImages';
-import miiViewer from '@/components/MiiViewer.vue';
-import MiiViewer from '@/components/MiiViewer.vue';
 usePreloadImages()
 </script>
 
 <template>
-  <MiiViewer />
+  <router-view />
 </template>
-
-<style scoped></style>
