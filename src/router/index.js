0f85bb27ef0e91fb62cf72aca71c2b68ce24db82
mathiiiiiis <mathis@mathiiis.de>
2026-03-18 08:41:31 +0100

    add own favicon and custom page title

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,19 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/Home.vue'),
+      meta: { title: 'Home' },
     },
     {
       path: '/projects',
       name: 'projects',
       component: () => import('@/views/Projects.vue'),
+      meta: { title: 'Projects' },
     },
   ],
+})
+
+router.afterEach((to) => {
+  document.title = `${to.meta.title || 'Home'} - mathiiis.de`
 })
 
 export default router
