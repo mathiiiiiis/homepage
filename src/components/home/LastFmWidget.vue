81accaf48d1c377accd044b722b0083444124ffa
mathis <mathiisprv@gmail.com>
2026-03-19 09:05:46 +0100

    Reduce blur effect on LastFmWidget background

--- src/components/home/LastFmWidget.vue
+++ src/components/home/LastFmWidget.vue
@@ -85,7 +85,7 @@ onUnmounted(() => {
   width: 100%;
   height: 100%;
   object-fit: cover;
-  filter: blur(40px) brightness(0.6) saturate(1.5);
+  filter: blur(20px) brightness(0.6) saturate(1.5);
   transform: scale(1.5);
   z-index: 0;
 }
