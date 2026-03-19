f3a51085eac3c473f4b9d2b8734f039e2f8dc79e
mathiiiiiis <mathis@mathiiis.de>
2026-03-19 13:54:18 +0100

    Remove unused files and fix typo in file name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import IconSprite from '@/components/icons/IconSprite.vue'
-import ThemeToogle from '@/components/ThemeToogle.vue'
+import ThemeToogle from '@/components/ThemeToggle.vue'
 import { usePreloadImages } from '@/composables/usePreloadImages'
 import { useTheme } from '@/composables/useTheme'
 usePreloadImages()
